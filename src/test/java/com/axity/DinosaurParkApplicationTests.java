3c7c7341617832e14af3810452a5adb8e57f25a8
B21NAYIB <nayibdelacruzmarquez@gmail.com>
2026-05-20 21:16:37 -0600

    fix: resolver dependencias de Spring Boot Test en pom.xml y actualizar clase de pruebas a JUnit 5

---
+++ src/test/java/com/axity/DinosaurParkApplicationTests.java
@@ -0,0 +1,14 @@
+package com.axity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinosaurParkApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Esta prueba verifica que el contexto de Spring Boot cargue correctamente
+    }
+
+}
